5dff79bedfa02b36f66c0a2fc386d613a50e5489
s.bogdanov <s.bogdanov@geekbrains.ru>
2021-10-27 10:17:35 +0300

    <master> files

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package main
 
+import "fmt"
+
 func main() {
-	cards := newDeck()
+	cards := newDeckFromFile("cards.txt")
 
-	hand, remainingDeck := deal(cards, 5)
-
-	hand.print()
-	remainingDeck.print()
+	fmt.Println(cards)
 }
