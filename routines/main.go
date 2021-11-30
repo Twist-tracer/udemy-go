0b7a263cbfe2a2831bcd1aa10a2a6423a4647dd2
twist-tracer <twist.tracer@gmail.com>
2021-11-30 23:46:33 +0300

    <master> refactoring

--- routines/main.go
+++ routines/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -20,19 +21,24 @@ func main() {
 		go checkLink(link, c)
 	}
 
-	for i := 0; i < len(links); i++ {
-		fmt.Println(<-c)
+	for link := range c {
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+
+			checkLink(link, c)
+		}(link)
 	}
 }
 
 func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
+
 	if err != nil {
 		fmt.Println(link, "might be down!")
-		c <- "Might ve down I think"
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "be up!")
-	c <- "Yes its up"
+	c <- link
 }
